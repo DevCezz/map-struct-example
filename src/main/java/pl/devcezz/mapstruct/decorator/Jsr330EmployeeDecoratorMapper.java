d49a071739ef35a12b43c93d30fe567ec4f5b84f
Cezary Sanecki <devcezz@gmail.com>
2022-03-18 15:21:02 +0100

    Added example of using component in mapper

--- src/main/java/pl/devcezz/mapstruct/decorator/Jsr330EmployeeDecoratorMapper.java
+++ src/main/java/pl/devcezz/mapstruct/decorator/Jsr330EmployeeDecoratorMapper.java
@@ -2,26 +2,21 @@ package pl.devcezz.mapstruct.decorator;
 
 import javax.inject.Inject;
 import javax.inject.Named;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.util.Locale;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public abstract class Jsr330EmployeeDecoratorMapper implements EmployeeMapper {
-
-    private static final String SALARY_FORMAT = "%1$.2f€";
-    private static final String SALARY_REGEX = "\\d+\\.\\d{2}";
 
     @Inject
     @Named("pl.devcezz.mapstruct.decorator.EmployeeMapperImpl_")
     private EmployeeMapper delegate;
 
+    @Inject
+    private SalaryService employeeService;
+
     @Override
     public EmployeeDto map(Employee employee) {
         EmployeeDto dto = delegate.map(employee);
         return dto.toBuilder()
-                .salaryWithEuro(formatSalary(employee.getSalary()))
+                .salaryWithEuro(employeeService.formatSalary(employee.getSalary()))
                 .build();
     }
 
@@ -29,18 +24,7 @@ public abstract class Jsr330EmployeeDecoratorMapper implements EmployeeMapper {
     public Employee map(EmployeeDto dto) {
         Employee employee = delegate.map(dto);
         return employee.toBuilder()
-                .salary(extractSalary(dto.getSalaryWithEuro()))
+                .salary(employeeService.extractSalary(dto.getSalaryWithEuro()))
                 .build();
-    }
-
-    private String formatSalary(BigDecimal salary) {
-        return String.format(Locale.US, SALARY_FORMAT, salary);
-    }
-
-    private BigDecimal extractSalary(String salary) {
-        Pattern pattern = Pattern.compile(SALARY_REGEX);
-        Matcher matcher = pattern.matcher(salary);
-        BigDecimal result = matcher.find() ? new BigDecimal(matcher.group(0)) : BigDecimal.ZERO;
-        return result.setScale(2, RoundingMode.HALF_UP);
     }
 }
